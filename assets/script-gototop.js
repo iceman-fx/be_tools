5b19e374e9d688c5199f2d7ad6d73016c872e451
Falko Müller <fdfm2009@gmail.com>
2023-06-28 08:02:33 +0200

    1.7.9

--- assets/script-gototop.js
+++ assets/script-gototop.js
@@ -1,17 +1,17 @@
 // BackendTools Scripts
-// v1.7.5
+// v1.7.9
 
 $(function(){
 	//add gotoTop-Button
 	$gt = $("div.bet-gototop");
 	$gt.click(function(e){
 		e.preventDefault();
-		$("body, html").animate({scrollTop: 0}, 650, "linear");
+		$("body, html").animate({scrollTop: 0}, 350, "linear");
 	});
 	
 	$(document).on('ready scroll', function(){
 		var pos = $(this).scrollTop();
-		if (pos > $(window).height()) {	
+		if (pos > 275) {	
 			$gt.addClass('bet-gototop-show');
 		} else {
 			$gt.removeClass('bet-gototop-show');
